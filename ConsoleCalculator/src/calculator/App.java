6ad2283f2f4ad8e676226de64c41c338661b3ba5
Harish Tiwari <harishtiwari1998@gmail.com>
2024-02-18 16:01:12 +0000

    Added operations

--- ConsoleCalculator/src/calculator/App.java
+++ ConsoleCalculator/src/calculator/App.java
@@ -1,7 +1,94 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Enter the choices..");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiply");
+        System.out.println("4. Divide");
+        System.out.println("5. Modulus");
+        System.out.println("6. Factorial");
+        System.out.println("7. Root");
+        System.out.println("8. Exponential");
+        System.out.println("9. Log");
+        System.out.println("10. Percentage");
+        System.out.println("11. Sine");
+        System.out.println("12. Cosine");
+        System.out.println("13. Show History");
+
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        Operations op = new Operations();
+        int num1 = 0;
+        int num2 = 0;
+
+        switch (choice) {
+            case 1:
+                System.out.println("Enter two number");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+
+                op.add(num1, num2);
+                break;
+
+            case 2:
+                System.out.println("Enter two number");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+
+                op.sub(num1, num2);
+                break;
+            case 3:
+                System.out.println("Enter two number");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+
+                op.multiply(num1, num2);
+                break;
+            case 4:
+                System.out.println("Enter two number");
+                num1 = sc.nextInt();
+                num2 = sc.nextInt();
+
+                op.divide(num1, num2);
+                break;
+            case 6:
+                System.out.println("Enter the number");
+                num1 = sc.nextInt();
+
+                op.factorial(num1);
+                break;
+            case 7:
+                System.out.println("Enter the number");
+                Double num = sc.nextDouble();
+
+                op.root(num);
+                break;
+            case 8:
+                System.out.println("Enter the base and power");
+                Double base = sc.nextDouble();
+                Double power = sc.nextDouble();
+
+                op.exponential(base, power);
+                break;
+            case 9:
+                System.out.println("Enter the argument and base");
+                Double arg = sc.nextDouble();
+
+                op.logarithm(arg);
+                break;
+            case 10:
+                System.out.println("Enter the number");
+                Double percent = sc.nextDouble();
+
+                op.percentage(percent);
+                break;
+
+            default:
+                break;
+        }
     }
 }
--- ConsoleCalculator/src/calculator/Operations.java
+++ ConsoleCalculator/src/calculator/Operations.java
@@ -1,38 +1,77 @@
 package calculator;
+
 /**
- *  add
-    sub
-    multiply
-    divide
-    modulus
-    factorial
-    root
-    exponential
-    log
-    percentage
-    sine 
-    cosine
-    conversions
+ * add
+ * sub
+ * multiply
+ * divide
+ * modulus
+ * factorial
+ * root
+ * exponential
+ * log
+ * percentage
+ * sine
+ * cosine
+ * conversions
  */
 public class Operations {
-    
-    public int add(int num1 , int num2){
-        return Math.addExact(num1, num2);
+
+    public void add(int num1, int num2) {
+        int sum = Math.addExact(num1, num2);
+        System.out.println("Sum = " + sum);
     }
 
-    public int sub(int num1 , int num2){
-        return Math.subtractExact(num1, num2);
+    public void sub(int num1, int num2) {
+        int diff = Math.subtractExact(num1, num2);
+        System.out.println("Diff = " + diff);
     }
 
-    public int multiply(int num1 , int num2){
-        return Math.multiplyExact(num1, num2);
+    public void multiply(int num1, int num2) {
+        int prod = Math.multiplyExact(num1, num2);
+        System.out.println("Product = " + prod);
     }
 
-    public int divide(int num1 , int num2){
-        if(num2 == 0){
-            return -1;
+    public void divide(int num1, int num2) {
+        if (num2 == 0) {
+            System.out.println("Divide By zero is not allowed");
         }
 
-        return num1 / num2;
+        int result = num1 / num2;
+        System.out.println("Result = " + result);
+    }
+
+    public void factorial(int num1) {
+        int result = fact(num1);
+
+        System.out.println("Factorial = " + result);
+    }
+
+    private int fact(int num1) {
+        if (num1 == 0 || num1 == 1) {
+            return 1;
+        }
+        return num1 * fact(num1 - 1);
+    }
+
+    public void root(double num) {
+        Double result = Math.sqrt(num);
+
+        System.out.println("Root = " + result);
+    }
+
+    public void exponential(double num1, double num2) {
+        Double result = Math.pow(num1, num2);
+
+        System.out.println("Exponential = " + result);
+    }
+
+    public void logarithm(Double arg) {
+        Double result = Math.log(arg);
+        System.out.println("Result = " + result);
+    }
+
+    public void percentage(Double percent) {
+        
     }
 }
